6c32bd095948cae075ace2004bfe3f5b20ab8eb6
itdenismaslyuk <itdenismaslyuk@gmail.com>
2021-02-24 21:45:27 +0700

    Add project files.

---
+++ View2/View2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BL;
+using Model;
+
+namespace View2
+{
+    
+    class View2
+    {
+        static void Main(string[] args)
+        {
+            Logic logic = new Logic();
+            foreach (Employee user in logic.GetDataDap())
+            {
+                Console.WriteLine(user.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
